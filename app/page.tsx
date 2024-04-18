70f19e896ca1ad1b4f331545c0146240905ad951
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-04-18 21:02:18 +0545

    layout updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-bold text-teal-500">Hello World</h1>;
+  return <h1 className="text-bold text-teal-500 text-2xl">Hello World</h1>;
 }
