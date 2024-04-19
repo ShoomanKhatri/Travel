ac28c36829c9076e5842a669214d9431936635c8
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-04-19 10:44:32 +0545

    completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,28 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import type { Metadata } from 'next';
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: "Travel",
-  description: "Travel app",
-};
+  title: 'Travel',
+  description: 'Travel UI/UX App for Camping',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        <main className="relative overflow-hidden">{children}</main>
-        
-        <Footer/>
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <h1 className="text-bold text-teal-500 text-2xl">Hello World</h1>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  )
 }
