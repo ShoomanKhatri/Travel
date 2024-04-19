1f13aa4b2a61b4c9e33dda49cd2fccd3bf97a38b
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-04-19 11:38:30 +0545

    updated again

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
-import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
@@ -9,10 +9,15 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image src="travel-now-logo.svg" alt="logo" width={100} height={34}/>
+            <Image
+              src="travel-now-logo.svg"
+              alt="logo"
+              width={100}
+              height={34}
+            />
           </Link>
 
-          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
             {FOOTER_LINKS.map((columns) => (
               <FooterColumn title={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
@@ -33,9 +38,7 @@ const Footer = () => {
                     key={link.label}
                     className="flex gap-4 md:flex-col lg:flex-row"
                   >
-                    <p className="whitespace-nowrap">
-                      {link.label}:
-                    </p>
+                    <p className="whitespace-nowrap">{link.label}:</p>
                     <p className="medium-14 whitespace-nowrap text-blue-70">
                       {link.value}
                     </p>
@@ -59,16 +62,18 @@ const Footer = () => {
         </div>
 
         <div className="border bg-gray-20" />
-        <p className="regular-14 w-full text-center text-gray-30">2023 TravelNow | All rights reserved</p>
+        <p className="regular-14 w-full text-center text-gray-30">
+          2024 TravelNow | All rights reserved
+        </p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
 type FooterColumnProps = {
   title: string;
   children: React.ReactNode;
-}
+};
 
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
@@ -76,7 +81,7 @@ const FooterColumn = ({ title, children }: FooterColumnProps) => {
       <h4 className="bold-18 whitespace-nowrap">{title}</h4>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
