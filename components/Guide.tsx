850b7b9eabc649c4f0a7d12c56001afa7dc5757f
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-04-19 11:37:32 +0545

    updated

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Guide = () => {
   return (
@@ -10,13 +10,21 @@ const Guide = () => {
           We are here for you
         </p>
         <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Guide You to Easy Path</h2>
-          <p className="regular-16 text-gray-30 xl:max-w-[520px]">Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+            Guide You to Easy Path
+          </h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+            Only with the travelnow application you will no longer get lost and
+            get lost again, because we already support offline maps when there
+            is no internet connection in the field. Invite your friends,
+            relatives and friends to have fun in the wilderness through the
+            valley and reach the top of the mountain
+          </p>
         </div>
       </div>
 
       <div className="flexCenter max-container relative w-full">
-        <Image 
+        <Image
           src="/boat.png"
           alt="boat"
           width={1440}
@@ -25,7 +33,7 @@ const Guide = () => {
         />
 
         <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
-          <Image 
+          <Image
             src="/meter.svg"
             alt="meter"
             width={16}
@@ -33,7 +41,7 @@ const Guide = () => {
             className="h-full w-auto"
           />
           <div className="flexBetween flex-col">
-            <div className='flex w-full flex-col'>
+            <div className="flex w-full flex-col">
               <div className="flexBetween w-full">
                 <p className="regular-16 text-gray-20">Destination</p>
                 <p className="bold-16 text-green-50">48 min</p>
@@ -41,15 +49,17 @@ const Guide = () => {
               <p className="bold-20 mt-2">Aguas Calientes</p>
             </div>
 
-            <div className='flex w-full flex-col'>
+            <div className="flex w-full flex-col">
               <p className="regular-16 text-gray-20">Start track</p>
-              <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">
+                Wonorejo Pasuruan
+              </h4>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Guide
+export default Guide;
